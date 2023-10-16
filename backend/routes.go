0e5bbdf0c6260daf3f6427fa6042936c94981301
Kemal - HN <krovcanin@harlemnext.com>
2023-10-16 09:19:39 +0200

    routes file changes

--- backend/routes.go
+++ backend/routes.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -14,15 +17,56 @@ func setupRoutes(r *mux.Router) {
 }
 
 type Word struct {
-	ID       int    `json:"id"`
-	Text     string `json:"text"`
-	Language string `json:"language"`
+	ID              int    `json:"id"`
+	Word            string `json:"word"`
+	WordType        string `json:"word_type"`
+	WordDescription string `json:"word_description"`
+	WordExample     string `json:"word_example"`
+	WordTranslation string `json:"word_translation"`
 }
 
-func NewWord(id int, text, language string) Word {
-	return Word{
-		ID:       id,
-		Text:     text,
-		Language: language,
+func getDutchWords(w http.ResponseWriter, r *http.Request) {
+	words := []Word{}
+
+	rows, err := db.Query("SELECT * FROM languages_words WHERE language = 'NL'")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var word Word
+		if err := rows.Scan(&word.ID, &word.Word, &word.WordType, &word.WordDescription, &word.WordExample, &word.WordTranslation); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		words = append(words, word)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(words)
+}
+
+func getEnglishWords(w http.ResponseWriter, r *http.Request) {
+	words := []Word{}
+
+	rows, err := db.Query("SELECT * FROM languages_words WHERE language = 'EN'")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var word Word
+		if err := rows.Scan(&word.ID, &word.Word, &word.WordType, &word.WordDescription, &word.WordExample, &word.WordTranslation); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		words = append(words, word)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(words)
 }
